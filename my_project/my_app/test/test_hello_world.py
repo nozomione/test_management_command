0102fd94aa5c6ea9e3cb9928effb94d18ff75d92
Nozomi Ichihara <nozomione.online@gmail.com>
2024-06-12 15:24:42 -0400

    fix TestHelloWorld

--- my_project/my_app/test/test_hello_world.py
+++ my_project/my_app/test/test_hello_world.py
@@ -1,15 +1,17 @@
 from django.test import TestCase
 from django.core.management import call_command
 from io import StringIO
+import sys
 
 
 class TestHelloWorld(TestCase):
     def call_command(self):
         out = StringIO()
+        sys.stdout = out
         call_command("hello_world", stdout=out)
-        return out.getvalue()
+        return out.getvalue().replace("\n", "")
 
     def test_hello_world(self):
-        out = self.call_command()
+        result = self.call_command()
         expected_value = "Hello World"
-        self.assertEquals(out, expected_value)
+        self.assertEquals(result, expected_value)
